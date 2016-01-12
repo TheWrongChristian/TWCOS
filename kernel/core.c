d5c34ebd196ef8a1b61095a156ac7b7f14e78e77
csmith <csmith@noemail.net>
2016-01-12 00:59:08 +0000

    Bootstrap allocator. Should be able at this point to start allocating pages for the main allocator, once the page allocation code is done (it is not yet done!).
    FossilOrigin-Name: c2b4abe6e860fe3a5af0f4599e9759c0d2dc566c

--- kernel/core.c
+++ kernel/core.c
@@ -1,22 +1,48 @@
+#include <stddef.h>
+
 #include <arch/arch.h>
 
 #include "core.h"
 
+extern char _bootstrap_nextalloc;
+static char * nextalloc = &_bootstrap_nextalloc;
 
+#define ALIGNMENT 16
+
+static void * localalloc(size_t size)
+{
+	void * m = (void*)nextalloc;
+	size += (ALIGNMENT-1);
+	size &= (~(ALIGNMENT-1));
+	nextalloc += size;
+
+	return m;
+}
+
+/*
+ * Up to 32 maps
+ */
+struct kernel_mmap {
+	uint32_t base;
+	int count;
+	int free;
+	uint32_t * map;
+};
+static int mmap_count = 0;
+static struct kernel_mmap mmap[32];
+
+void page_add(uint32_t base, uint32_t count)
+{
+	mmap[mmap_count].base = base;
+	mmap[mmap_count].count = count;
+	mmap[mmap_count].free = count;
+	mmap[mmap_count].map = localalloc(sizeof(*mmap[mmap_count].map) * count);
+	mmap_count++;
+}
 
 #if INTERFACE
 
 #define page_to_ptr(page) (void*)((long)page * ARCH_PAGE_SIZE)
 
-struct kernel_mmap {
-	uint32_t pbase;
-	int pages;
-	int inuse;
-	uint8_t * freemap;
-}
 #endif
 
-/*
- * Up to 32 maps
- */
-static struct kernel_mmap mmap[32];
