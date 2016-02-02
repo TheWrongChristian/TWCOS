2ca8bb9723eeffe1887a4d2934935c60f0d17cb4
csmith <csmith>
2016-02-02 18:46:56 +0000

    Use .S files for assembler, to allow cpp use.

--- build/tools.mk
+++ build/tools.mk
@@ -15,7 +15,7 @@ include $(TOP)/build/param.mk
 COPTS=-g
 CFLAGS=$(COPTS) -std=gnu99 -ffreestanding -Wall -I$(TOP)/arch/$(ARCH)/include -I$(TOP)/include
 
-OBJS=$(SRCS_S:.s=.o) $(SRCS_C:.c=.o)
+OBJS=$(SRCS_S:.S=.o) $(SRCS_C:.c=.o)
 
 .PHONY: clean
 
