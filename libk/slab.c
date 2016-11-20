f7293ff4e4716b88661ae59de28a988675147c02
csmith <csmith>
2016-11-20 12:28:59 +0000

    Slower, conservative bit scan, will optimize when appropriate.
    Add some more test code.

--- libk/slab.c
+++ libk/slab.c
@@ -75,14 +75,23 @@ void * slab_alloc(slab_type_t * stype)
 				/* There is some available slots */
 				int slot = i*32;
 				uint32_t a = slab->available[i];
+				uint32_t mask = 0x80000000;
+#if 0
+				if (a & 0x0000ffff) slot += 16, a >>= 16;
+				if (a & 0x00ff00ff) slot += 8, a >>= 8;
+				if (a & 0x0f0f0f0f) slot += 4, a >>= 4;
+				if (a & 0x33333333) slot += 2, a >>= 2;
+				if (a & 0x55555555) slot += 1, a >>= 1;
+#endif
+				while(mask) {
+					if (a&mask) {
+						break;
+					}
+					mask>>=1;
+					slot++;
+				}
 
-				if (a & 0x0000ffff) slot += 16;
-				if (a & 0x00ff00ff) slot += 8;
-				if (a & 0x0f0f0f0f) slot += 4;
-				if (a & 0x33333333) slot += 2;
-				if (a & 0x55555555) slot += 1;
-
-				slab->available[i] &= ~(0x80000000 >> (slot-1));
+				slab->available[i] &= ~(0x80000000 >> (slot));
 
 				return (char*)(slab+1) + slab->esize*slot;
 			}
@@ -114,13 +123,21 @@ void slab_test()
 {
 	slab_type_t t;
 	slab_type_t * t2;
-	void * p;
+	void * p[4];
 
 	slab_type_create(&t, sizeof(t));
 	t2 = slab_alloc(&t);
 	slab_type_create(t2, 1270);
-	p = slab_alloc(t2);
 
-	slab_free(p);
+	p[0] = slab_alloc(t2);
+	p[1] = slab_alloc(t2);
+	p[2] = slab_alloc(t2);
+	p[3] = slab_alloc(t2);
+
+	slab_free(p[3]);
+	slab_free(p[2]);
+	slab_free(p[1]);
+	slab_free(p[0]);
+
 	slab_free(t2);
 }
