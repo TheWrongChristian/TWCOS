37b7c5dca7ada17053994fcf7abaa98321a4a814
csmith <csmith@noemail.net>
2016-12-13 20:41:47 +0000

    Start threading
    FossilOrigin-Name: 7c0f23fd08bfaac53fb6ad0987715226877946a0

--- arch/i386/subdir.mk
+++ arch/i386/subdir.mk
@@ -2,3 +2,7 @@ ARCH_SRCS_S := $(subdir)/asm.S $(subdir)/setjmp.S
 ARCH_SRCS_C := $(subdir)/console.c $(subdir)/multiboot.c $(subdir)/init.c $(subdir)/vmap.c $(subdir)/i386.c $(subdir)/pci.c
 SRCS_S += $(ARCH_SRCS_S)
 SRCS_C += $(ARCH_SRCS_C)
+
+$(subdir)/asm.S: $(subdir)/isr.inc
+$(subdir)/isr.inc: $(subdir)/isr.sh
+	sh $< > $@
