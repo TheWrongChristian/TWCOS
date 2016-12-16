e4c93f551c0341d6d34849ea02b5e5df4ead40c3
csmith <csmith>
2016-12-16 13:16:44 +0000

    Start of locking. Not working yet.

--- kernel/thread.c
+++ kernel/thread.c
@@ -8,11 +8,20 @@
 typedef int tls_key;
 
 #define TLS_MAX 32
-typedef struct {
+typedef struct thread_s {
+	/* Runtime data */
 	void * tls[TLS_MAX];
 	arch_context_t context;
+
+	/* Run state */
+	tstate state;
+
+	/* Queue */
+	struct thread_s *prev;
+	struct thread_s *next;
 } thread_t;
 
+enum tstate { THREAD_NEW, THREAD_RUNNABLE, THREAD_RUNNING, THREAD_SLEEPING };
 
 #endif
 
@@ -46,6 +55,16 @@ void * tls_get(int key)
 
 static slab_type_t threads[1];
 
+#define LOCK_COUNT 64
+static struct lock_s {
+	int spin;
+	void * p;
+	int count;
+	int condcount;
+	thread_t * owner;
+	thread_t * waiting;
+} locks[LOCK_COUNT];
+
 void thread_init()
 {
 	slab_type_create(threads, sizeof(thread_t));
@@ -60,6 +79,112 @@ thread_t * thread_fork()
 	}
 
 	return thread;
+}
+
+static int lockcount;
+static int contended;
+
+static struct lock_s * thread_lock_hash(void * p)
+{
+	int hash = ((ptri)p * 2047) & (LOCK_COUNT-1);
+	int nexthash = hash;
+	struct lock_s * lock = locks+hash;
+
+	/*  */
+	while(1) {
+		if (spin_trylock(&lock->spin)) {
+			/* Got the lock, test if it's in use */
+			if (0 == lock->count) {
+				lock->p = p;
+				return lock;
+			} else if (p == lock->p) {
+				return lock;
+			} else {
+				/* Lock in use by another pointer */
+				contended++;
+				thread_lock_wait(lock);
+			}
+		}
+		/* FIXME: yield */
+	}
+}
+
+static int thread_trylock_internal(struct lock_s * lock, void * p)
+{
+	if (0 == lock->count) {
+		/* Not in use, mark it as used and locked */
+		lock->count = 1;
+		lock->owner = arch_get_thread();
+		spin_unlock(&lock->spin);
+		return 1;
+	} else if (lock->owner == arch_get_thread()) {
+		/* Recursive lock, increase count */
+		lock->count++;
+		spin_unlock(&lock->spin);
+		return 1;
+	}
+
+	return 0;
+}
+
+int thread_trylock(void * p)
+{
+	struct lock_s * lock = thread_lock_hash(p);
+
+	return thread_trylock_internal(lock, p);
+}
+
+void thread_lock(void * p)
+{
+	struct lock_s * lock = thread_lock_hash(p);
+
+	while(1) {
+		if (thread_trylock_internal(lock, p)) {
+			return;
+		} else {
+			/* Owned by someone else */
+			thread_lock_wait(lock);
+		}
+	}
+}
+
+void thread_unlock(void *p)
+{
+	struct lock_s * lock = thread_lock_hash(p);
+
+	if (lock->owner == arch_get_thread()) {
+		/* We own the lock, unlock it */
+		lock->count--;
+		if (0 == lock->count && lock->waiting) {
+			thread_lock_signal(lock);
+		}
+	} else {
+		kernel_panic("Unlocking unowned lock\n");
+	}
+	spin_unlock(&lock->spin);
+}
+
+void thread_wait(void *p)
+{
+	struct lock_s * lock = thread_lock_hash(p);
+
+	if (lock->owner == arch_get_thread()) {
+		int count = lock->count;
+		lock->count = 0;
+		thread_lock_wait(lock);
+		while(1) {
+			if (thread_trylock_internal(lock, p)) {
+				/* We have the lock again, restore count */
+				lock->count = count;
+				return;
+			} else {
+				/* Owned by someone else */
+				thread_lock_wait(lock);
+			}
+		}
+	} else {
+		kernel_panic("Unlocking unowned lock\n");
+	}
 }
 
 void thread_test()
