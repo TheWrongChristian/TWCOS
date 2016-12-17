d176584305162a8d39e36c77131539d7b49c937c
csmith <csmith>
2016-12-17 02:15:18 +0000

    Added a primitive RR scheduler.

--- libk/subdir.mk
+++ libk/subdir.mk
@@ -1,2 +1,2 @@
-SRCS_C += $(subdir)/stream.c $(subdir)/exception.c $(subdir)/slab.c $(subdir)/string.c
+SRCS_C += $(subdir)/stream.c $(subdir)/exception.c $(subdir)/slab.c $(subdir)/string.c $(subdir)/list.c
 # SRCS_C += $(LIBK_SRCS_C)
