6bfb7727cfb4894ae224fe41ca05d60a273aa5de
csmith <csmith@noemail.net>
2016-12-21 03:21:24 +0000

    Adding exceptions for check calls.
    FossilOrigin-Name: 933b59c7076e0c6fc5b3aa366eaad9374203bd60

--- kernel/check.c
+++ kernel/check.c
@@ -1,15 +1,19 @@
 #include "check.h"
 
+struct exception_def exception_def_CheckException = { "CheckException", &exception_def_Exception };
+struct exception_def exception_def_NullCheckException = { "NullCheckException", &exception_def_CheckException };
+struct exception_def exception_def_IntBoundsException = { "IntBoundsException", &exception_def_CheckException };
+
 void check_not_null(void * p, const char * error)
 {
 	if (0 == p) {
-		kernel_panic("%s: %p\n", error, p);
+		KTHROWF(NullCheckException, "%s: %p\n", error, p);
 	}
 }
 
 void check_int_bounds(int i, int low, int high, const char * error)
 {
 	if (i<low || i>high) {
-		kernel_panic("%s: %d\n", error, i);
+		KTHROWF(IntBoundsException, "%s: %d\n", error, i);
 	}
 }
