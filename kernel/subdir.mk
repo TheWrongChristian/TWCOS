ef293dcc377b2bd2a991fdedc2fa49f41c86c9c8
csmith <csmith@noemail.net>
2016-12-30 01:43:07 +0000

    Move linker script and resulting kernel to the arch subdirectory.
    FossilOrigin-Name: 7959842d20467aaaef5eaab6d9dce7393b6bc845

--- kernel/subdir.mk
+++ kernel/subdir.mk
@@ -1,8 +1,1 @@
 SRCS_C += $(subdir)/main.c  $(subdir)/core.c  $(subdir)/pci.c  $(subdir)/printk.c  $(subdir)/panic.c $(subdir)/thread.c $(subdir)/check.c
-
-kernel: $(subdir)/kernel
-
-LD_SCRIPT := $(subdir)/linker.ld
-
-$(subdir)/kernel: $(OBJS) $(LD_SCRIPT)
-	$(CC) -T $(LD_SCRIPT) -o $@ -ffreestanding -O2 -nostdlib $(OBJS) -lgcc
