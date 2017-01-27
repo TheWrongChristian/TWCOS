9226f9114c7e04df235d08f3a7bb21dca3a252b5
csmith <csmith>
2017-01-27 00:48:09 +0000

    Separate shared kernel address space.

--- arch/i386/i386.c
+++ arch/i386/i386.c
@@ -520,7 +520,7 @@ void arch_thread_init(thread_t * thread)
 	}
 	map_t * as = arch_get_thread()->as = tree_new(0, TREE_TREAP);
 	segment_t * heap = vm_segment_anonymous(_bootstrap_nextalloc, 64 * 0x100000, SEGMENT_W, 0);
-	map_put(as, MAP_PKEY(heap), heap);
+	map_put(kas, MAP_PKEY(heap), heap);
 }
 
 void arch_idle()
