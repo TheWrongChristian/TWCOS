f49a4de655808d2331743df59d00ffa81619fb5c
csmith <csmith>
2017-05-07 00:40:58 +0000

    Move vector API to map.

--- libk/vector.c
+++ libk/vector.c
@@ -9,9 +9,6 @@ typedef struct vector_s {
 	struct vector_table_s * table;
 } vector_t;
 
-typedef void (*vector_walk_func)(vector_t * v, void * arg, map_key i, intptr_t d);
-typedef void (*vector_walkp_func)(vector_t * v, void * arg, map_key i, void * p);
-
 #endif
 
 #define VECTOR_TABLE_ENTRIES_LOG2 6
@@ -72,8 +69,9 @@ static void vector_checksize(vector_t * v, map_key i)
 	}
 }
 
-intptr_t vector_put(vector_t * v, map_key i, intptr_t d)
+static map_data vector_put(map_t * m, map_key i, map_data d)
 {
+	vector_t * v = (vector_t*)m;
 	vector_checksize(v, i);
 	intptr_t * entry = vector_entry_get(v->table, i, 1);
 	intptr_t old = *entry;
@@ -81,13 +79,9 @@ intptr_t vector_put(vector_t * v, map_key i, intptr_t d)
 	return old;
 }
 
-void * vector_putp(vector_t * v, map_key i, void * p)
+static map_data vector_get(map_t * m, map_key i)
 {
-	return (void*)vector_put(v, i, (intptr_t)p);
-}
-
-intptr_t vector_get(vector_t * v, map_key i)
-{
+	vector_t * v = (vector_t*)m;
 	intptr_t * entry = vector_entry_get(v->table, i, 0);
 
 	if (entry) {
@@ -97,17 +91,38 @@ intptr_t vector_get(vector_t * v, map_key i)
 	return 0;
 }
 
-void * vector_getp(vector_t * v, map_key i)
+static void vector_walk_table(vector_t * v, vector_table_t * t, void * arg, int base, walk_func f)
 {
-	return (void*)vector_get(v, i);
+	for(int i=0; i<VECTOR_TABLE_ENTRIES; i++) {
+		if (t->d[i]) {
+			if (t->level) {
+				vector_walk_table(v, (vector_table_t *)t->d[i], arg, (base+i)*VECTOR_TABLE_ENTRIES, f);
+			} else {
+				f(arg, base+i, t->d[i]);
+			}
+		}
+	}
+}
+
+static void vector_walk(map_t * m, walk_func f, void * arg )
+{
+	vector_t * v = (vector_t*)m;
+	if (v->table) {
+		vector_walk_table(v, v->table, arg, 0, f);
+	}
+}
+
+static void vector_test_walk(void * ignored, map_key i, void * p)
+{
+	kernel_printk("v[%d] = %p\n", i, p);
 }
 
 map_t * vector_new()
 {
 	vector_t * v = slab_calloc(vectors);
-	static struct map_ops tree_ops = {
+	static struct map_ops vector_ops = {
                 destroy: 0,
-                walk: tree_walk,
+                walk: vector_walk,
                 put: vector_put,
                 get: vector_get,
                 get_le: 0,
@@ -116,45 +131,10 @@ map_t * vector_new()
                 iterator: 0 /* vector_iterator */
         };
 
+	v->ops = &vector_ops;
 	v->table = vector_table_new(0);
 
-	return v;
-}
-
-static void vector_walk_table(vector_t * v, vector_table_t * t, void * arg, int base, vector_walk_func fi, vector_walkp_func fp)
-{
-	for(int i=0; i<VECTOR_TABLE_ENTRIES; i++) {
-		if (t->d[i]) {
-			if (t->level) {
-				vector_walk_table(v, t->d[i], arg, (base+i)*VECTOR_TABLE_ENTRIES, fi, fp);
-			} else {
-				if (fi) {
-					fi(v, arg, base+i, t->d[i]);
-				} else {
-					fp(v, arg, base+i, (void*)t->d[i]);
-				}
-			}
-		}
-	}
-}
-
-void vector_walk(vector_t * v, vector_walk_func f, void * arg )
-{
-	if (v->table) {
-		vector_walk_table(v, v->table, arg, 0, f, 0);
-	}
-}
-
-void vector_walkp(vector_t * v, vector_walkp_func f, void * arg )
-{
-	if (v->table) {
-		vector_walk_table(v, v->table, arg, 0, 0, f);
-	}
-}
-
-static void vector_test_walk(vector_t * v, void * ignored, map_key i, void * p)
-{
-	kernel_printk("v[%d] = %p\n", i, p);
+	return (map_t*)v;
 }
 
 void vector_test()
@@ -165,21 +145,21 @@ void vector_test()
 	map_t * v = vector_new();
 	void * p = vector_test;
 
-	kernel_printk("v[%d] = %p\n", i, vector_getp(v, i));
-	vector_putp(v, i, p);
-	kernel_printk("v[%d] = %p\n", i, vector_getp(v, i));
+	kernel_printk("v[%d] = %p\n", i, map_getip(v, i));
+	map_putip(v, i, p);
+	kernel_printk("v[%d] = %p\n", i, map_getip(v, i));
 
 	i+=VECTOR_TABLE_ENTRIES;
 
-	kernel_printk("v[%d] = %p\n", i, vector_getp(v, i));
-	vector_putp(v, i, p);
-	kernel_printk("v[%d] = %p\n", i, vector_getp(v, i));
+	kernel_printk("v[%d] = %p\n", i, map_getip(v, i));
+	map_putip(v, i, p);
+	kernel_printk("v[%d] = %p\n", i, map_getip(v, i));
 
 	i*=VECTOR_TABLE_ENTRIES;
 
-	kernel_printk("v[%d] = %p\n", i, vector_getp(v, i));
-	vector_putp(v, i, p);
-	kernel_printk("v[%d] = %p\n", i, vector_getp(v, i));
+	kernel_printk("v[%d] = %p\n", i, map_getip(v, i));
+	map_putip(v, i, p);
+	kernel_printk("v[%d] = %p\n", i, map_getip(v, i));
 
-	vector_walkp(v, vector_test_walk, 0);
+	map_walkip(v, vector_test_walk, 0);
 }
