bb72eb3bf669d4e33bcb43e980c2c8a0f284f731
csmith <csmith@noemail.net>
2017-11-15 23:17:33 +0000

    Remove allocation of vnodes. They'll be managed by individual filesystems.
    FossilOrigin-Name: 4f9acc821695c8bdf7cb0912dd6009b6e91a0386

--- kernel/vfs.c
+++ kernel/vfs.c
@@ -4,43 +4,39 @@
 
 #include <stddef.h>
 
-typedef struct vnode_ops_s {
-	page_t (*get_page)( vnode_s * vnode, off_t offset);
-	void (*put_page)( vnode_s * vnode, off_t offset, page_t page );
-	void (*close)( vnode_s * vnode );
-} vnode_ops_t;
+typedef int64_t inode_t;
 
-typedef struct fs_ops_s {
+typedef struct vnode_ops_s vnode)_ops_t;
+struct vnode_ops_s {
+	fs_t * (*get_fs)(vnode_t * vnode);
+	page_t (*get_page)( vnode_t * vnode, off_t offset);
+	void (*put_page)( vnode_t * vnode, off_t offset, page_t page );
+	inode_t (*namei)(vnode_t * dir, const char * name);
+	void (*close)( vnode_t * vnode );
+};
+
+typedef struct fs_ops_s fs_ops_t;
+struct fs_ops_s {
 	void (*close)(fs_t * fs);
-	vnode_t * (*get_vnode)(vnode_t * dir, const char * name);
-	vnode_t * (*get_root_vnode)();
-} fs_ops_t;
+	vnode_t * (*get_vnode)(fs_t * fs, inode_t inode);
+	void (*put_vnode)(vnode_t * vnode);
+	vnode_t * (*open)(vnode_t * dev);
+};
 
 enum vnode_type { VNODE_REGULAR, VNODE_DIRECTORY, VNODE_DEV, VNODE_FIFO, VNODE_SOCKET };
 
-typedef struct vnode_s {
-	struct fs_s * fs;
+typedef struct vnode_s vnode_t;
+struct vnode_s {
+	vnode_ops_t * vnops;
+	fs_t * fs;
 	int ref;
+};
 
-	vnode_type type;
-	union {
-		struct {
-			size_t size;
-
-			void * fsnode;
-		} regular;
-		struct {
-			struct vnode_s * parent;
-			void * fsnode;
-		} dir;
-	} u;
-} vnode_t;
-
+typedef struct fs_s fs_t;
 typedef struct fs_s {
-	vnode_ops_s * vnops;
 	fs_ops_s * fsops;
-} fs_t;
-
+};
+ 
 #endif
 
 exception_def FileException = { "FileException", &Exception };
@@ -118,37 +114,4 @@ void vfs_vnode_close(vnode_t * vnode)
 void vfs_fs_close(vnode_t * root)
 {
 	root->fs->fsops->close(root->fs);
-}
-
-static void vnode_mark(void * p)
-{
-	vnode_t * vnode = p;
-	slab_gc_mark(vnode->fs);
-}
-
-static void vnode_finalize(void * p)
-{
-}
-
-static slab_type_t vnodes[1] = {SLAB_TYPE(sizeof(vnodes), vnode_mark, vnode_finalize)};
-
-static vnode_t * vfs_vnode_alloc(fs_t * fs, vnode_type type)
-{
-	vnode_t * vnode = slab_alloc(vnodes);
-
-	vnode->type = type;
-	/* vnode->ops = (fs) ? fs->vnops : 0; */
-	vnode->fs = fs;
-
-	return vnode;
-}
-
-vnode_t * vfs_vnode_directory(fs_t * fs, void * fsnode)
-{
-	return vfs_vnode_alloc(fs, VNODE_DIRECTORY);
-}
-
-vnode_t * vfs_vnode_regular(fs_t * fs, void * fsnode)
-{
-	return vfs_vnode_alloc(fs, VNODE_REGULAR);
 }
