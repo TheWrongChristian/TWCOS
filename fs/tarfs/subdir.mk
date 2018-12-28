d919b7b03e4552f9fbaf3ef0a1f5c70b1ca25873
csmith <csmith@noemail.net>
2018-12-28 00:46:20 +0000

    Added some test ascii art files.
    FossilOrigin-Name: ff9c46161478f1ad9d22f0142eb533291bd0cc18

--- fs/tarfs/subdir.mk
+++ fs/tarfs/subdir.mk
@@ -1,6 +1,7 @@
 SRCS_C += $(subdir)/tarfs.c $(subdir)/tarfs.tar.c
 
-TARFS_FILES := $(SRCS_KERNEL_C) $(SRCS_LIBK_C)
+SRCS_VT_FILES = shell/aart
+TARFS_FILES := $(SRCS_KERNEL_C) $(SRCS_LIBK_C) $(SRCS_VT_FILES)
 TARFS_TAR := $(subdir)/tarfs.tar
 TARFS_TAR_C := $(subdir)/tarfs.tar.c
 TARFS_DEEPDIR := $(subdir)/a/deep/directory/
