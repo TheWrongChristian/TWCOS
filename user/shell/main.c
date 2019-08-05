c3d75ef6923160933137524f41679ca5be77fa33
csmith <csmith@noemail.net>
2019-08-05 22:41:03 +0000

    Simpler output.
    FossilOrigin-Name: e69350cfdfc68e0ec1bf4f1636aefd0388071dc4

--- user/shell/main.c
+++ user/shell/main.c
@@ -2,14 +2,17 @@
 
 int main(void)
 {
+#if 0
 	printf("Hello world\n");
+#endif
 	if (fork()) {
 		int status;
 		while(1 == getpid()) {
 			waitpid(0, &status, 0);
 		}
 	} else {
-		printf("Hello world from pid %d\n", getpid());
+		printf("Hello world from pid %d\r", getpid());
+		fflush(stdout);
 		execve("/user/shell/init", 0, 0);
 	}
 
