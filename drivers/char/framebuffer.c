0331d9abf580c80d78f52a3f31868baa1919f5de
csmith <csmith@noemail.net>
2019-12-23 19:08:09 +0000

    D'oh, forgot framebuffer driver code!
    FossilOrigin-Name: 97136947ce13460f0c39c113bcace78c71ad6333

---
+++ drivers/char/framebuffer.c
@@ -0,0 +1,21 @@
+#include "framebuffer.h"
+
+#if INTERFACE
+
+#endif
+
+void * fb_create(uintptr_t addr, size_t width, size_t height, size_t pitch, size_t type, size_t bpp)
+{
+	/* Physical page address */
+	page_t paddr = addr >> ARCH_PAGE_SIZE_LOG2;
+
+	/* FB size */
+	size_t fbsize = PTR_ALIGN_NEXT(pitch*height, ARCH_PAGE_SIZE);
+
+	/* Memory */
+	void * p = vm_kas_get_aligned(fbsize, ARCH_PAGE_SIZE);
+	segment_t * segfb = vm_segment_direct(p, fbsize, SEGMENT_R | SEGMENT_W, paddr);
+	vm_kas_add(segfb);
+
+	return p;
+}
