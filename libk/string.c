53f94964f91e084873db49e43500e1bb2067b137
csmith <csmith@noemail.net>
2020-04-25 17:16:56 +0000

    Actually step through the memory!
    FossilOrigin-Name: 661464d1329f07490aca06d1fc181680410bd854

--- libk/string.c
+++ libk/string.c
@@ -89,9 +89,9 @@ int memcmp(const void *s1, const void *s2, size_t n)
 	const char * c1 = s1;
 	const char * c2 = s2;
 	for(int i=0; i<n; i++) {
-		if (*c1<*c2) {
+		if (c1[i]<c2[i]) {
 			return -1;
-		} else if (*c1>*c2) {
+		} else if (c1[i]>c2[i]) {
 			return 1;
 		}
 	}
