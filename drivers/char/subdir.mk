f9df18561525988ec82769d31f21e27336ae3cad
csmith <csmith@noemail.net>
2020-05-03 00:30:11 +0000

    Generate font.c
    FossilOrigin-Name: 7e0277a353f59d8028db80ec567c6f981e9ca34b

--- drivers/char/subdir.mk
+++ drivers/char/subdir.mk
@@ -1,5 +1,7 @@
 SRCS_DRIVERS_CHAR_C := $(subdir)/terminal.c $(subdir)/ns16550.c $(subdir)/framebuffer.c $(subdir)/font.c
 FONT := $(subdir)/font
 
+includes:: $(FONT).c
+
 $(FONT).c: $(FONT).psf
 	xxd -i $(FONT).psf $(FONT).c
