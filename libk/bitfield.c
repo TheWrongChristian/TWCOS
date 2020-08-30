cb07b8965582ddea97cd3bc122712d153fd7b00c
csmith <csmith@noemail.net>
2020-08-30 23:52:45 +0000

    Fix bitset, and add bitget.
    FossilOrigin-Name: f22749ca3a94386a48c52cb2304f9dada1ca84d2

--- libk/bitfield.c
+++ libk/bitfield.c
@@ -8,8 +8,15 @@
 uint32_t bitset(uint32_t flags, int start, int len, int value)
 {
 	uint32_t mask = ~(~0 << len);
-	flags &= ~(mask << (start - len));
-	flags |= (value << (start - len));
+	flags &= ~(mask << (1+start - len));
+	flags |= (value << (1+start - len));
 
 	return flags;
+}
+
+uint32_t bitget(uint32_t flags, int start, int len)
+{
+	uint32_t mask = ~(~0 << len);
+
+	return (flags>>(start-len+1)) & mask;
 }
