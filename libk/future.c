fdfa2f76a83243a93eff7ef3a32e3fb483799ddf
csmith <csmith@noemail.net>
2021-03-03 00:19:02 +0000

    Additions to help chaining of futures, as well as timeouts.
    FossilOrigin-Name: 96220519e3e17b64e405ccbc5eec6d8bce9604a6

--- libk/future.c
+++ libk/future.c
@@ -7,8 +7,10 @@ struct future_t {
 	volatile int pending;
 	exception_cause * cause;
 	intptr_t status;
+	intptr_t success;
 	void (*cleanup)(void * p);
 	void * p;
+	future_t * prev;
 };
 
 
@@ -17,11 +19,17 @@ struct future_t {
 exception_def FutureException = { "FutureException", &Exception };
 exception_def CancelledFutureException = { "CancelledFutureException", &FutureException };
 
-intptr_t future_get(future_t * future)
+intptr_t future_get_timeout(future_t * future, timerspec_t timeout)
 {
+	if (future->prev) {
+		/* Ignore status from previous futures */
+		(void)future_get_timeout(future->prev, timeout);
+	}
+
 	INTERRUPT_MONITOR_AUTOLOCK(future->lock) {
 		while(future->pending) {
-			interrupt_monitor_wait(future->lock);
+			TRACE();
+			interrupt_monitor_wait_timeout(future->lock, timeout);
 		}
 	}
 
@@ -37,12 +45,19 @@ intptr_t future_get(future_t * future)
 	return future->status;
 }
 
+intptr_t future_get(future_t * future)
+{
+	return future_get_timeout(future, 0);
+}
+
 void future_set(future_t * future, intptr_t status)
 {
 	INTERRUPT_MONITOR_AUTOLOCK(future->lock) {
-		future->status = status;
-		future->pending = 0;
-		interrupt_monitor_broadcast(future->lock);
+		if (future->pending) {
+			future->status = status;
+			future->pending = 0;
+			interrupt_monitor_broadcast(future->lock);
+		}
 	}
 }
 
@@ -52,6 +67,7 @@ void future_init(future_t * future, void (*cleanup)(void * p), void * p)
 	future->pending = 1;
 	future->cleanup = cleanup;
 	future->p = p;
+	future->prev = 0;
 }
 
 future_t * future_create(void (*cleanup)(void * p), void * p)
@@ -64,7 +80,17 @@ future_t * future_create(void (*cleanup)(void * p), void * p)
 void future_cancel(future_t * future, exception_cause * cause)
 {
 	INTERRUPT_MONITOR_AUTOLOCK(future->lock) {
-		future->pending = 0;
-		future->cause = cause;
+		if (future->pending) {
+			future->pending = 0;
+			future->cause = cause;
+			interrupt_monitor_broadcast(future->lock);
+		}
+	}
+}
+
+void future_chain(future_t * future, future_t * prev)
+{
+	INTERRUPT_MONITOR_AUTOLOCK(future->lock) {
+		future->prev = prev;
 	}
 }
